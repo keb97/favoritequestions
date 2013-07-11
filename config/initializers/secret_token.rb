4c92b2844a0911d24f9cb9603596d0ecd9847c9b
Kathryn <keb97@cornell.edu>
2013-07-10 21:01:40 -0700

    Generate favorite questions application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Favoritequestions::Application.config.secret_token = '9bac4789577e73c49555627b7a81667148ef1668c81773793e736bb3b3b0fc8b9b969657852f05fdda3d3267aa39928026faf25d02fd8eccf4baba41cdc1516c'
